8300c198a8f002ec02fff6c326bfca3a5f3e894f
Mikhayla <mikhayla.stephenson@gmail.com>
2023-01-25 13:55:27 +1000

    fun additions

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -1,10 +1,35 @@
 import { useState } from "react"
-import { Progress, Button, Heading, Mark, useHighlight } from "@chakra-ui/react"
+import {
+  Progress,
+  Button,
+  Heading,
+  Mark,
+  useHighlight,
+  Icon,
+  Box,
+  useToast,
+} from "@chakra-ui/react"
+import { FaSkull } from "react-icons/fa"
 
 export default function Hello() {
+  const toast = useToast()
+
   const [yearsWorked, setYearsWorked] = useState(0)
+
+  const avgLifeSpan = 5
+  const lives = Math.floor(yearsWorked / avgLifeSpan)
+  let livesArray = []
+  for (let i = 0; i < lives; i++) {
+    livesArray.push(<Icon as={FaSkull} />)
+  }
   const billGatesMoney = 145757154000
   const averageAnnualIncome = 68250
+  const progressBarTotal = 100000000
+
+  const currentMoney = yearsWorked * averageAnnualIncome
+  const bars = Math.floor(currentMoney / progressBarTotal)
+  const numBars = Math.round(billGatesMoney / progressBarTotal)
+
   const chunks = useHighlight({
     text: "Bill Gates has $103 billion",
     query: ["$103 billion"],
@@ -13,6 +38,7 @@ export default function Hello() {
     text: "The average Australian makes $68,250",
     query: ["$68,250"],
   })
+
   return (
     <div>
       <Heading
@@ -24,6 +50,7 @@ export default function Hello() {
           if (!match) return text
           return text === "" ? (
             <Box
+              key={text}
               as="u"
               fontFamily="NewYork"
             >
@@ -31,6 +58,7 @@ export default function Hello() {
             </Box>
           ) : (
             <Mark
+              key={text}
               bg="black"
               color="white"
               fontFamily="NewYork"
@@ -51,6 +79,7 @@ export default function Hello() {
           if (!match) return text
           return text === "" ? (
             <Box
+              key={text}
               as="u"
               fontFamily="NewYork"
             >
@@ -58,6 +87,7 @@ export default function Hello() {
             </Box>
           ) : (
             <Mark
+              key={text}
               bg="yellow"
               color="black"
               fontFamily="NewYork"
@@ -69,12 +99,28 @@ export default function Hello() {
           )
         })}
       </Heading>
-
       <h2>Become a self made Billionaire</h2>
-
       <Button
         onClick={() => {
           setYearsWorked(yearsWorked + 1)
+          if (yearsWorked === avgLifeSpan - 1) {
+            toast({
+              title: "You Died",
+              description: "The average Australian lives 83 years",
+              status: "error",
+              duration: 2000,
+              isClosable: true,
+            })
+          }
+          if (bars === 1 && !alerted) {
+            toast({
+              title: "You earned you first $100 million!",
+              description: "only " + numBars + " to go!",
+              status: "Success",
+              duration: 5000,
+              isClosable: true,
+            })
+          }
         }}
         bgColor="white"
         colorScheme="black"
@@ -83,6 +129,9 @@ export default function Hello() {
       >
         Work
       </Button>
+      <p>
+        {bars} / {numBars}
+      </p>
       <Heading
         as="h3"
         size="xl"
@@ -95,12 +144,16 @@ export default function Hello() {
           px="2"
           py="1"
         >
-          ${averageAnnualIncome * yearsWorked}
+          ${currentMoney}
         </Mark>
       </Heading>
       <Progress
-        value={((averageAnnualIncome * yearsWorked) / billGatesMoney) * 100}
+        value={
+          ((currentMoney - bars * progressBarTotal) / progressBarTotal) * 100
+        }
       />
+      <div>{yearsWorked} years spent</div>
+      <Box mt="5">{livesArray}</Box>{" "}
     </div>
   )
 }
